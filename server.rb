a290fee5f78172a8fc7210fc6e0371ad7db06c02
zywoon93 <38690332+zywoon93@users.noreply.github.com>
2018-04-27 22:40:04 +0900

    making block

--- server.rb
+++ server.rb
@@ -10,7 +10,12 @@ get '/'do
 message = ""
 
  b.all_chains.each do |a|
+ 	
  	message << "번호는 : " +a["index"].to_s + "<br>"
+ 	message << "nonce는 : " +a["nonce"].to_s + "<br>"
+ 	message << "시간은 : " +a["time"].to_s + "<br>"
+ 	message << "앞주소는 : " +a["previous_address"].to_s + "<br>"
+ 	message << "내주소는 : " +Digest::SHA256.hexdigest(a.to_s) + "<br>"
  	message << "<hr>"
 end
 
