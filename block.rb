8260d9e24c9d0ee7fceba3580f4263734c204351
zywoon93 <38690332+zywoon93@users.noreply.github.com>
2018-04-27 21:23:54 +0900

    history

--- block.rb
+++ block.rb
@@ -1,11 +1,13 @@
 class Blockchain
 	def mining
 
+	history = []	
 		begin
 			nonce = rand(100000) #rand()
+			history << nonce
 			hashed = Digest::SHA256.hexdigest(nonce.to_s)# number to letter
         end while hashed[0..3] != "0000"
 
-    nonce
+    history.size
 	end
 end
