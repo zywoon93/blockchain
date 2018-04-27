b2e62271ed12fe9bf595be2c9b651ba0bb9b3535
zywoon93 <38690332+zywoon93@users.noreply.github.com>
2018-04-27 21:09:44 +0900

    hashed

--- block.rb
+++ block.rb
@@ -2,8 +2,9 @@ class Blockchain
 	def mining
 
 		begin
-			nonce = rand(10000) #rand()
-        end while Digest::SHA256.hexdigst(nonce.to_s) +
+			nonce = rand(100000) #rand()
+			hashed = Digest::SHA256.hexdigest(nonce.to_s)# number to letter
+        end while hashed[0..3] != "0000"
 
     nonce
 	end
