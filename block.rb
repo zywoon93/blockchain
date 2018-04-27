46ed596c71fb2f38c6edf73686fef3c014e64ac7
zywoon93 <38690332+zywoon93@users.noreply.github.com>
2018-04-27 20:56:09 +0900

    SHA256

--- block.rb
+++ block.rb
@@ -1,1 +1,10 @@
-aa
+class Blockchain
+	def mining
+
+		begin
+			nonce = rand(10000) #rand()
+        end while Digest::SHA256.hexdigst(nonce.to_s) +
+
+    nonce
+	end
+end
--- server.rb
+++ server.rb
@@ -1,1 +1,17 @@
-bb
+# encoding: UTF-8
+
+require 'sinatra'
+require './block'
+
+b = Blockchain.new
+
+
+get '/'do 
+"It's block list."
+	
+end
+
+get '/mine' do
+	b.mining.to_s
+	
+end
