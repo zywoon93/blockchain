c60b61c362cc49d899c779d94f50199821759cb7
zywoon93 <38690332+zywoon93@users.noreply.github.com>
2018-04-27 21:50:31 +0900

    block

--- block.rb
+++ block.rb
@@ -1,4 +1,8 @@
 class Blockchain
+	def initialize 
+		@chain = []
+	end
+
 	def mining
 
 	history = []	
@@ -7,6 +11,13 @@ class Blockchain
 			history << nonce
 			hashed = Digest::SHA256.hexdigest(nonce.to_s)# number to letter
         end while hashed[0..3] != "0000"
+
+    block = {
+	"index" => 1,
+	"time" => 20329384,
+	"nonce" => 3234
+}
+
 
     history.size
 	end
