49abcee88f004ac0dec26c106824359160bc6b71
zywoon93 <38690332+zywoon93@users.noreply.github.com>
2018-04-28 23:11:16 +0900

    3번째 수업

--- block.rb
+++ block.rb
@@ -1,32 +1,44 @@
-class Blockchain
-	def initialize 
-		@chain = []
+class Blockchain #class 다음의 문자는 꼭 대문자
+
+	def initialize  
+		@chain = [] #@처음부터 끝까지 관통하는 속성 
 	end
+
+
 
 	def mining
+		history = [] 
+		current_time = Time.now.to_f
 
-	history = []	
-		begin
-			nonce = rand(100000) #rand()
-			history << nonce
-			hashed = Digest::SHA256.hexdigest(nonce.to_s)# number to letter
-        end while hashed[0..3] != "0000"
+		begin                      #begin-end 무한반복 로직 
+			nonce = rand(100000)
+			history << nonce #<<는 .push와 같다 			 
+		    hashed = Digest::SHA256.hexdigest(nonce.to_s) #숫자 암호화
+		end while hashed[0..3] != '0000'#0000으로 암호화된 숫자 찾기 
+		nonce
 
-    block = {
-	"index" => @chain.size + 1,
-	"time" => Time.now,
-	"nonce" => nonce,
-	"previous_address" => Digest::SHA256.hexdigest(last_block.to_s)
-	}
-     @chain << block
-    block
+		block = {
+             "index" => @chain.size + 1,
+             "time" => Time.now,
+             "nonce" => nonce,
+             "previous_address" => Digest::SHA256.hexdigest(last_block.to_s)
+		}
+
+		@chain << block
+		block
 	end
 
-    def last_block
-	@chain[-1]
-    end
+        def last_block
+        @chain[-1]
+        end
 
-    def all_chains
-    	@chain
-    end
+        def all_blocks
+        	@chain
+        end
+
 end
+
+
+
+
+
--- server.rb
+++ server.rb
@@ -1,27 +1,29 @@
-# encoding: UTF-8
+require 'sinatra' #시나트라가 필요하다 
+require './block' #블럭파일을 불러와라 
 
-require 'sinatra' 
-require './block' 
-
-b = Blockchain.new
+aaaa = Blockchain.new
 
 
-get '/'do 
-message = ""
 
- b.all_chains.each do |a|
- 	
- 	message << "번호는 : " +a["index"].to_s + "<br>"
- 	message << "nonce는 : " +a["nonce"].to_s + "<br>"
- 	message << "시간은 : " +a["time"].to_s + "<br>"
- 	message << "앞주소는 : " +a["previous_address"].to_s + "<br>"
- 	message << "내주소는 : " +Digest::SHA256.hexdigest(a.to_s) + "<br>"
- 	message << "<hr>"
+ get'/' do 
+
+message = "<center>"
+
+
+aaaa.all_blocks.each do |b| #.each블럭을 하나씩 
+	message << "Number :" + b['index'].to_s + "<br>" 
+	message << "Time :" + b['Time'].to_s + "<br>" 
+	message << "Nonce :" + b['nonce'].to_s + "<br>" 
+	message << "Pre-Address :" + b['previous_address'].to_s + "<br>"
+	message << "My-Address :" +Digest::SHA256.hexdigest(b.to_s) + "<br>"  
+	message << "<hr>" # hr은 줄긋기 
+#Digest::SHA256 암호화
 end
 
-message
+message + "</center>"
+
 end
 
-get '/mine' do
-	b.mining.to_s	
-end
+get '/mine' do 
+aaaa.mining.to_s	# def mine의 결과를 브라우져에 표현해라  
+ end
