5570093143619ca953329be5dfd1eef746a8b8a5
zywoon93 <38690332+zywoon93@users.noreply.github.com>
2018-05-01 21:27:56 +0900

    transaction

--- block.rb
+++ block.rb
@@ -1,31 +1,44 @@
+require 'digest'
+
 class Blockchain #class 다음의 문자는 꼭 대문자
 
 	def initialize  
-		@chain = [] #@처음부터 끝까지 관통하는 속성 
+		@chain = []
+		@trans = []
+		 #@처음부터 끝까지 관통하는 속성 
+	end
+
+	def make_a_trans(s, r, a)
+		trans = {
+			"sender" => s,
+			"recv" => r,
+			"amount" => a
+
+		}
+		@trans << trans #저장
+		"다음 블럭에 쓰여집니다." + (@chain.length + 1).to_s
 	end
 
 
-
 	def mining
-		history = [] 
-		current_time = Time.now.to_f
-
+		
 		begin                      #begin-end 무한반복 로직 
 			nonce = rand(100000)
-			history << nonce #<<는 .push와 같다 			 
+		 
 		    hashed = Digest::SHA256.hexdigest(nonce.to_s) #숫자 암호화
 		end while hashed[0..3] != '0000'#0000으로 암호화된 숫자 찾기 
-		nonce
+		
 
 		block = {
-             "index" => @chain.size + 1,
-             "time" => Time.now,
+             "index" => @chain.size,
+             "time" => Time.now.to_i,
              "nonce" => nonce,
-             "previous_address" => Digest::SHA256.hexdigest(last_block.to_s)
+             "previous_address" => Digest::SHA256.hexdigest(last_block.to_s),
+		     "transaction" => @trans #블럭에 거래정보 박제 
 		}
-
+        @trans = [] # 새로운 블럭을 만들어라 
 		@chain << block
-		block
+	
 	end
 
         def last_block
--- server.rb
+++ server.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require 'sinatra' #시나트라가 필요하다 
 require './block' #블럭파일을 불러와라 
 
@@ -8,15 +9,14 @@ aaaa = Blockchain.new
  get'/' do 
 
 message = "<center>"
-
-
 aaaa.all_blocks.each do |b| #.each블럭을 하나씩 
 	message << "Number :" + b['index'].to_s + "<br>" 
 	message << "Time :" + b['Time'].to_s + "<br>" 
 	message << "Nonce :" + b['nonce'].to_s + "<br>" 
 	message << "Pre-Address :" + b['previous_address'].to_s + "<br>"
 	message << "My-Address :" +Digest::SHA256.hexdigest(b.to_s) + "<br>"  
-	message << "<hr>" # hr은 줄긋기 
+    message << "transaction information: "+ b["transaction"].to_s + "<br>"
+	message << "<hr>" # <hr>은 줄긋기 
 #Digest::SHA256 암호화
 end
 
@@ -26,4 +26,8 @@ end
 
 get '/mine' do 
 aaaa.mining.to_s	# def mine의 결과를 브라우져에 표현해라  
+ end
+
+ get '/trans' do
+ 	aaaa.make_a_trans(params["sender"], params["recv"], params["amount"])
  end
