cf14bbcb004dbc5308714a14b991840f8b89988f
zywoon93 <38690332+zywoon93@users.noreply.github.com>
2018-05-04 22:13:00 +0900

    node

--- block.rb
+++ block.rb
@@ -1,83 +1,100 @@
- 
- require 'securerandom'
- require 'digest'
 
- class BlockHeader
+require 'securerandom'
+require 'digest'
+require 'httparty'
 
- 	def initialize
- 		@time
- 		@prevBlockHash
- 		@nonce
- 		@height
- 	end
+class Blockchain #class 다음의 문자는 꼭 대문자
 
- 	def prevBlockInfo
- 	end
+	def initialize  
+		@chain = []
+		@trans = []
+		@wallet = {} 
+		@node =[]
+		
+	end
 
- end
+	def add_port(port)
+		@node << port
+	end
 
- class Blockchain
-
- 	def initialize
- 		@chain = []
- 		@transaction = []
- 		@wallet = {}
- 	end
-
- 	def show_all_wallet
- 		@wallet
- 	end
-
- 	def make_a_wallet
- 	    address = SecureRandom.uuid.gsub("-", "")
- 	    @wallet[address] = 100
- 	    @wallet
- 	end
-
- 	def make_a_trans(sender, receiver, amount)
- 		if 		@wallet[sender].nil?
- 				"보내는 주소가 잘못되었습니다."
- 		elsif 	@wallet[reciever].nil?
- 				"받는 주소가 잘못되었습니다."
- 		elsif   @wallet[sender].to_f < amount.to_f
- 			    "돈이 부족합니다." 
- 		else
- 				@wallet[sender]   = @wallet[sender].to_f - amount.to_f
- 				@wallet[receiver] = @wallet[receiver].to_f + amount.to_f
-
- 		trans = {
- 				"sender" => sender,
- 				"recv"   => receiver,
- 				"amount" => amount
- 				}
- 		@transaction << trans
- 				"다음 블록에 쓰여집니다." + (@chain.length + 1).to_s
- 		end
-    end
-
-    def mining
-
-    	begin
-        		nonce  = rand(1000000)
-				hashed = Digest::SHA256.hexdigest(nonce.to_s)
-		end while hashed[0..3] != "0000"
-		block = {
-				"nHeight" 			=> @chain.size,
-				"nTime"	  			=> Time.now.to_i,
-				"nNonce"  			=> nonce,
-				"previous_address" 	=> Digest::SHA256.hexdigest(last_block.to_s),
-				"Transaction"		=> @transaction 
-				}		         		
-		@transaction =[]
-		@chain << block
+	def all_node
+		@node
+	end
+	
+	def ask_block
+		@node.each do |n|
+			n_block = HTTParty.get("http://localhost:"+n+"/number_of_blocks")
+		if @chain.length < n_block.to_i
+			@chain = []
 		end
 	end
-
-	def last_block
-		@chain[-1]
+end
+	
+	def show_all_wallet
+		@wallet
 	end
 
-	def all_chains
-		@chain
+    
+    def make_a_new_wallet # 새로운 지갑을 만들어라 
+      address = SecureRandom.uuid.gsub("-", "") #gsub("-", "")은 하이픈을 공백으로 바꿔준다.
+      @wallet[address] = 1000 # 지갑에 1000원을 넣어라.
+      @wallet
+      end	
+
+	def make_a_trans(s, r, a) #(누가 누구에게 얼마를)
+      if @wallet[s].nil? #보내는 지갑에 돈이 없다면 
+      	"보내는 주소가 잘못되었습니다."
+      elsif @wallet[r].nil? #받는 지갑에 돈이 없다면 
+        "받는 주소가 잘못되었습니다."	
+      elsif @wallet[s].to_f < a.to_f #보내는 사람이 가진  돈이 보내는 돈 보다 작다면
+        "돈이 없습니다." 
+      else
+        
+        @wallet[s] = @wallet[s].to_f - a.to_f 
+        @wallet[r] = @wallet[r].to_f + a.to_f #.to_f는 유리수화 하라는 뜻
+
+		trans = {
+        "sender" => s,
+        "receiver" => r,
+        "amount" => a
+		}
+        @trans << trans
+        @trans
+	   end	
 	end
+
+	def mining
+		
+		begin                      #begin-end 무한반복 로직 
+			nonce = rand(100000)
+		 
+		    hashed = Digest::SHA256.hexdigest(nonce.to_s) #숫자 암호화
+		end while hashed[0..3] != '0000'#0000으로 암호화된 숫자 찾기 
+		
+
+		block = {
+             "index" => @chain.size,
+             "time" => Time.now.to_i,
+             "nonce" => nonce,
+             "previous_address" => Digest::SHA256.hexdigest(last_block.to_s),
+		     "transaction" => @trans #블럭에 거래정보 박제 
+		    }
+		    @trans = []
+		    @chain << block
+	
+	end
+
+        def last_block
+        @chain[-1]
+        end
+
+        def all_chains
+        	@chain
+        end
+
+end
+#uuid 특징 : 짧은 시간에 특수한 값을 만듬 uuid ruby는 SecureRandom 프로그램을 사용함.
+
+
+
 
--- server.rb
+++ server.rb
@@ -3,40 +3,65 @@
 require 'sinatra'
 require './block'
 
+set :port, 4568
+
 blockchain = Blockchain.new
 
-get '/' do 
-	message = "<center>"
-
-	blockchain.all_blocks.each do |b|
-		message << "BlockHeight	 	   : " + b['nHeight'].to_s + "<br>"
-		message << "Time			   : " + b['nTime'].to_s + "<br>"
-		message << "Nonce			   : " + b['nNonce'].to_s + "<br>"
-		message << "Previous_BlockHash : " + b['previous_address'].to_s + "<br>"
-		message << "Cur_BlockHash	   : " + Digest::SHA256.hexdigest(b.to_s) + "<br>"
-		message << "Cur_Transation	   : " + b['Transaction'].to_s + "<br>"
-		message << "<hr>"
-	end
-
-	message + "</center>"
-	message
+get '/add_node' do
+	port = params["port"]
+	blockchain.add_port(port)
 end
 
-get '/mine' do
-	blockchain.mining.to_s
+get '/all_node' do
+	blockchain.all_node.to_s
+end
+	
+get '/number_of_blocks' do #블럭의 갯수
+  blockchain.all_chains.size.to_s
+end
+ 
+get '/ask' do
+  blockchain.ask_block.to_s
 end
 
-get '/trans' do
-	blockchain.make_a_trans(params["sender"], params["recv"], params["amount"])
+ get '/' do 
+
+    message = "<center>"
+
+    blockchain.all_chains.each do |b| #.each ->블럭을 하나씩 
+
+	message << "Number      :" + b['index'].to_s + "<br>" #<br> 한줄 띄기 
+	message << "Time        :" + b['Time'].to_s + "<br>" 
+	message << "Nonce       :" + b['nonce'].to_s + "<br>" 
+	message << "Pre-Address :" + b['previous_address'].to_s + "<br>"
+	message << "My-Address  :" + Digest::SHA256.hexdigest(b.to_s) + "<br>"  
+    message << "Transactions:" + b['transaction'].to_s + "<br>"
+	message << "<hr>" # <hr>은 줄긋기 
+                      #Digest::SHA256 암호화
+    end
+
+    message + "</center>"
+    message 
 end
 
-get '/new_wallet' do
-	blockchain.make_a_wallet.to_s
+get '/mine' do 
+    blockchain.mining.to_s	# def mine의 결과를 브라우져에 표현해라  
+ end
+
+
+ get '/trans' do #trans 로직을 돌려라 
+ 	blockchain.make_a_trans(params["sender"], params["recv"], params["amount"]).to_s 
+ 
 end
 
-get 'wallet_list' do
-	blockchain.show_all_wallet.to_s
-end
+ get '/new_wallet' do 
+    blockchain.make_a_new_wallet.to_s	
+ end
+
+ get '/all_wallet' do
+ 	blockchain.show_all_wallet.to_s
+ end	
 
 
-
+#http://localhost:4567/trans?sender=a&recv=b&amount=1.1 
+#주                  소/trans?sender=보내는 사람 지갑주소&recv=받는 사람지갑주소&amount=보내는 양
