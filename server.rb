cc77ada38a5dcb8866906f1a77265e8a90792a72
zywoon93 <zywoon@naver.com>
2018-05-08 23:19:44 +0900

    first blockchain
    원조 블록

--- server.rb
+++ server.rb
@@ -3,28 +3,13 @@
 require 'sinatra'
 require './block'
 
-set :port, 4568
+
 
 blockchain = Blockchain.new
 
-get '/add_node' do
-	port = params["port"]
-	blockchain.add_port(port)
-end
 
-get '/all_node' do
-	blockchain.all_node.to_s
-end
-	
-get '/number_of_blocks' do #블럭의 갯수
-  blockchain.all_chains.size.to_s
-end
- 
-get '/ask' do
-  blockchain.ask_block.to_s
-end
 
- get '/' do 
+get '/' do 
 
     message = "<center>"
 
@@ -46,22 +31,51 @@ end
 
 get '/mine' do 
     blockchain.mining.to_s	# def mine의 결과를 브라우져에 표현해라  
- end
+end
 
 
- get '/trans' do #trans 로직을 돌려라 
+get '/trans' do #trans 로직을 돌려라 
  	blockchain.make_a_trans(params["sender"], params["recv"], params["amount"]).to_s 
  
 end
 
- get '/new_wallet' do 
+get '/new_wallet' do 
     blockchain.make_a_new_wallet.to_s	
- end
+end
 
- get '/all_wallet' do
+get '/all_wallet' do
  	blockchain.show_all_wallet.to_s
- end	
+end	
 
+get '/wallet_list' do
+  blockchain.wallet_list.to_s
+end
+
+get '/number_of_blocks' do #블럭의 갯수
+  blockchain.all_chains.size.to_s
+end
+
+get '/ask' do
+  blockchain.ask_block.to_s
+end  
+
+get '/add_node' do
+  port = params["port"]
+  blockchain.add_port(port)
+end
+
+get '/all_node' do
+  blockchain.all_node.to_s
+end
+
+get '/recv_chain' do
+  recv_chain = params["chain"]
+  extracted = JSON.parse(recv_chain)
+  blockchain.add_block(extracted)
+end  
+  
+
+ 
 
 #http://localhost:4567/trans?sender=a&recv=b&amount=1.1 
 #주                  소/trans?sender=보내는 사람 지갑주소&recv=받는 사람지갑주소&amount=보내는 양
