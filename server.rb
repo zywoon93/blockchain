021f1a200f5e32d56e67f1f4eeda64f68e19210e
zywoon93 <38690332+zywoon93@users.noreply.github.com>
2018-05-11 01:16:28 +0900

    simple

--- server.rb
+++ server.rb
@@ -1,26 +1,24 @@
 # encoding: UTF-8
 
 require 'sinatra'
-require './block'
+require './block'# block 파일을 불러오라
 
 
 
 blockchain = Blockchain.new
 
-
-
 get '/' do 
 
     message = "<center>"
 
-    blockchain.all_chains.each do |b| #.each ->블럭을 하나씩 
+    blockchain.all_blocks.each do |b| #.each ->블럭을 하나씩 
 
 	message << "Number      :" + b['index'].to_s + "<br>" #<br> 한줄 띄기 
 	message << "Time        :" + b['Time'].to_s + "<br>" 
 	message << "Nonce       :" + b['nonce'].to_s + "<br>" 
 	message << "Pre-Address :" + b['previous_address'].to_s + "<br>"
 	message << "My-Address  :" + Digest::SHA256.hexdigest(b.to_s) + "<br>"  
-    message << "Transactions:" + b['transaction'].to_s + "<br>"
+  message << "Transactions:" + b['transaction'].to_s + "<br>"
 	message << "<hr>" # <hr>은 줄긋기 
                       #Digest::SHA256 암호화
     end
